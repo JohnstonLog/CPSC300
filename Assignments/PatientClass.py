20f2f7ea96cc4f8710cfd01dccd6c82f33050f1f
hdaye <123016908+hdaye@users.noreply.github.com>
2024-10-25 13:09:37 -0700

    doctor class change

--- Assignments/PatientClass.py
+++ Assignments/PatientClass.py
@@ -5,9 +5,9 @@ class Patient:
   def __init__(self, patient_id, arrival_time, patient_type, treatment_time, wait_time, treatment_start_time):
     self.patient_id = patient_id                         # patient ID from global variable
     self.arrival_time = arrival_time                     # Read from file
-    self.patient_type = patient_type                     # Read From file
+    self.patient_type = patient_type                     # Read From file (E or W)
     self.treatment_time = treatment_time                 # Read from file
-    self.priority = random.randint(1, 5)                 # From random
+    self.priority = 1 if patient_type == 'E' else random.randint(1,5) # Priority 1 if type E else random 1-5
     self.wait_time = wait_time                           # calculated later
     self.treatment_start_time = treatment_start_time     # determined later
 
