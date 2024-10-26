82b29cdf812c72ea0dbcb6000992c10bceabc603
hdaye <123016908+hdaye@users.noreply.github.com>
2024-10-26 12:25:37 -0700

    read_patient_data

--- Assignments/Hospital.py
+++ Assignments/Hospital.py
@@ -1,1 +1,17 @@
+import PatientClass as Patient
+from queue import PriorityQueue
 
+def read_patient_data(file_name):
+    patients = []
+    with open(file_name, 'r') as file:
+        patient_id = 28064212
+        wait_time = 0
+        treatment_start_time = 0
+        for line in file:
+            arrival_time, patient_type, treatment_time = line.split()
+            treatment_time = int(treatment_time)
+            # Create a Patient object
+            patient = Patient(patient_id, arrival_time, patient_type, treatment_time, wait_time, trear=treatment_start_time)
+            patients.append(patient)
+            patient_id += 1  # Increment patient number
+    return patients
