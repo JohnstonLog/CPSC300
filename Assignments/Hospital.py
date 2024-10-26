b21a0ebb915a633519c7b2faefe2977cc6da4962
hdaye <123016908+hdaye@users.noreply.github.com>
2024-10-26 16:18:11 -0700

    arrival event and hospital update

--- Assignments/Hospital.py
+++ Assignments/Hospital.py
@@ -1,8 +1,8 @@
-import PatientClass as Patient
+from PatientClass import Patient
 from queue import PriorityQueue
 
-def read_patient_data(file_name):
-    patients = []
+# change read patient data so that it creates an arrival event and immediatly pushes to event queue
+def create_arrival_event(file_name):
     with open(file_name, 'r') as file:
         patient_id = 28064212
         wait_time = 0
@@ -11,7 +11,16 @@ def read_patient_data(file_name):
             arrival_time, patient_type, treatment_time = line.split()
             treatment_time = int(treatment_time)
             # Create a Patient object
-            patient = Patient(patient_id, arrival_time, patient_type, treatment_time, wait_time, trear=treatment_start_time)
-            patients.append(patient)
+            patient = Patient(patient_id, arrival_time, patient_type, treatment_time, wait_time, treatment_start_time)
             patient_id += 1  # Increment patient number
-    return patients
+    return arrival_event
+
+#main body
+
+event_queue = PriorityQueue() #initialize event queue
+
+fileName = 'Assignments\\data3.txt'
+patients = read_patient_data(fileName)
+
+for patient in patients:
+    print(patient.arrival_time, patient.patient_type)
